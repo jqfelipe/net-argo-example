67221e452621ad2e991cbb0d8cacc6a58b1fc7f0
Felipe Jiménez <jqfelipe@gmail.com>
2026-01-19 15:31:09 +0000

    Se crea folder del proyecto

---
+++ ImagenTarjeta.Api/Program.cs
@@ -0,0 +1,49 @@
+
+using ImagenTarjeta.Api.Interfaces;
+using ImagenTarjeta.Api.Models;
+using ImagenTarjeta.Api.Services;
+
+namespace ImagenTarjeta.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.Configure<List<TarjetaMapping>>(builder.Configuration.GetSection("Mapping"));
+            builder.Services.AddScoped<ITarjetasService, TarjetaService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+           
+
+            app.MapGet("/imagentarjeta", (ITarjetasService tarjetasService, HttpContext httpContext, string?bin, string?clase) =>
+            {                
+                var url = tarjetasService.GetUrlTarjeta(bin, clase);
+                return url;                
+            })
+            .WithName("ImagenTarjeta")
+            .WithOpenApi();
+
+            app.Run();
+        }
+    }
+}
---
+++ ImagenTarjeta.Api/Services/TarjetaService.cs
@@ -0,0 +1,39 @@
+﻿using ImagenTarjeta.Api.Interfaces;
+using ImagenTarjeta.Api.Models;
+using Microsoft.Extensions.Options;
+
+namespace ImagenTarjeta.Api.Services
+{
+    public class TarjetaService : ITarjetasService
+    {
+        public readonly IEnumerable<TarjetaMapping> listaTarjetas;
+
+        public TarjetaService(IOptions<List<TarjetaMapping>> configTarjetas)
+        {
+            listaTarjetas = configTarjetas.Value;
+        }
+
+        public string GetUrlTarjeta(string? bin, string? clase)
+        {
+            int claseDefault = 999;
+            string url = string.Empty;
+            if (!string.IsNullOrEmpty(bin))
+            {
+                _ = int.TryParse(bin, out int binNumber);
+                url = listaTarjetas.FirstOrDefault(p => p.Bin == binNumber)?.UrlImagen ?? string.Empty;
+            }
+            else if (!string.IsNullOrEmpty(clase))
+            {
+                _ = int.TryParse(clase, out int claseNumber);
+                url = listaTarjetas.FirstOrDefault(p => p.TipoTarjeta == claseNumber)?.UrlImagen ?? string.Empty;
+            }
+
+            if (string.IsNullOrEmpty(url))
+            {
+                url = listaTarjetas.FirstOrDefault(p => p.TipoTarjeta == claseDefault)?.UrlImagen ?? string.Empty;
+            }
+            
+            return url;
+        }
+    }
+}
